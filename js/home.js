93c909be595a7b69f52a5e491036ab005b7da8a5
Hpyorange <jaspercheng2022@gmail.com>
2022-11-06 22:47:26 -0500

    add switch language function (unfinished))

--- js/home.js
+++ js/home.js
@@ -2,7 +2,7 @@ const hamburger = document.querySelector('.header .nav-bar .nav-list .hamburger'
 const mobile_menu = document.querySelector('.header .nav-bar .nav-list ul');
 const menu_item = document.querySelectorAll('.header .nav-bar .nav-list ul li a');
 const header = document.querySelector('.header.container');
-
+const switchLanguage = document.querySelector('#switch')
 
 hamburger.addEventListener('click', () => {
 	hamburger.classList.toggle('active');
@@ -24,3 +24,97 @@ menu_item.forEach((item) => {
 		mobile_menu.classList.toggle('active');
 	});
 });
+
+
+ 
+const switchLan = () =>{
+
+	const nav1 = document.querySelector('.nav-list ul li:nth-child(1) a')
+	const nav2 = document.querySelector('.nav-list ul li:nth-child(2) a')
+	const nav3 = document.querySelector('.nav-list ul li:nth-child(3) a')
+	const nav4 = document.querySelector('.nav-list ul li:nth-child(4) a')
+
+	const heroContent1 = document.querySelector('#hero h1:nth-child(1)')
+	const heroContent2 = document.querySelector('#hero h1:nth-child(2)')
+	const heroContent3 = document.querySelector('#hero h1:nth-child(3)')
+	const heroContentBtn = document.querySelector('#hero a')
+	const wel = document.querySelector('#hero h2')
+	const animate = document.querySelector('#animate')
+
+	const aboutContent1 = document.querySelector('#about .section-title')
+	const aboutBtn = document.querySelector('#about a')
+	
+	const project = document.querySelector('#projectHeader')
+	const projectSub1=document.querySelector('#subHeader1')
+	const projectSub2=document.querySelector('#subHeader2')
+
+	const ContactHeader=document.querySelector('#contactHeader');
+	const phone =document.querySelector('#phone')
+	const email =document.querySelector('#email')
+	const location =document.querySelector('#location')
+
+	const toZh = ()=>{
+		nav1.textContent='主页'
+		nav2.textContent='关于我'
+		nav3.textContent='项目'
+		nav4.textContent='联系方式'
+		heroContent1.textContent='嗨，'
+		heroContent2.textContent='我叫'
+		heroContent3.textContent='程乐章'
+		heroContentBtn.textContent='作品集'
+		wel.innerHTML='欢迎来到我的宇宙'
+		animate.style.visibility = 'visible'
+
+		aboutContent1.innerHTML='关于<span>我</span>'
+		aboutBtn.textContent='简历下载'
+
+		project.innerHTML='近期<span>作品</span>'
+		projectSub1.innerHTML='<span>UI/UX</span>作品'
+		projectSub2.innerHTML='<span>编程</span>作品'
+
+		ContactHeader.innerHTML='联系<span>方式</span>'
+		phone.innerHTML='<h1>电话</h1><h2>13488696656</h2>'
+		email.innerHTML='<h1>电子邮箱</h1><h2>yuemoon45@hotmail.com</h2>'
+		location.innerHTML='<h1>住址地区</h1><h2>北京</h2>'
+	}
+
+	const toEn = ()=>{
+		nav1.textContent='Home'
+		nav2.textContent='ABOUT ME'
+		nav3.textContent='Projects'
+		nav4.textContent='Contact me'
+		heroContent1.textContent='Hi,'
+		heroContent2.textContent='My Name is'
+		heroContent3.textContent='Jasper'
+		heroContentBtn.textContent='Portfolio'
+		wel.innerHTML=`Welcome To My Universe`
+
+		aboutContent1.innerHTML='about <span>me</span>'
+		aboutBtn.textContent='Download Resume'
+
+		project.innerHTML='Recent <span>Projects</span>'
+		projectSub1.innerHTML='<span>UI/UX</span> Projects'
+		projectSub1.innerHTML='<span>Coding</span> Projects'
+
+		ContactHeader.innerHTML='Contact <span>info</span>'
+		phone.innerHTML='<h1>Phone</h1><h2>+1(437)788-1280</h2>'
+		email.innerHTML='<h1>Email</h1><h2>jaspercheng2022@gmail.com</h2>'
+		location.innerHTML=`<h1>Location</h1><h2>Toronto,Canada</h2>`
+
+	}
+	
+
+
+	if(nav1.textContent=='Home'){
+		toZh();
+	}
+	else {
+		toEn();	
+	}
+
+
+
+
+}
+
+switchLanguage.addEventListener('click',switchLan);
